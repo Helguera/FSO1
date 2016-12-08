97855d0b2b01d119b049c17ff0965bb284ab72a4
Javier Helguera <javier.helguera@alumnos.uva.es>
2016-12-08 21:20:31 +0100

    Creado el productor

---
+++ Practica_2016.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+sem_t availableSpaceBuffer1;
+sem_t availableDataBuffer1;
+sem_t availableSpaceBuffer2;
+sem_t availableDataBuffer2;
+
+int main(){
+   printf("hola");
+   return(0);
+}
