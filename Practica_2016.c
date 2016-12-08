66fce9e0563a3938049f32b9269229e99f5b5c5f
Javier Helguera <javier.helguera@alumnos.uva.es>
2016-12-08 21:23:06 +0100

    Ahora si, añadido el productor

--- Practica_2016.c
+++ Practica_2016.c
@@ -4,12 +4,92 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+int **buffer1;
+int **buffer2;
+int tamBuffer1;
 
 sem_t availableSpaceBuffer1;
 sem_t availableDataBuffer1;
 sem_t availableSpaceBuffer2;
 sem_t availableDataBuffer2;
+
+// Words in file counter
+int wordsCounter = 0;
+// Final number of file words
+int numberOfWords;
+
+
+//------------------------------------------------------------------------------
+
+void *productor(void *arg) {
+  // Declare variable which represents next position to fill in the
+  // circular buffer1.
+  int nextToFillBuffer1 = 0;
+  // Productor loop
+  while (1) {
+
+    int numeros=0;
+
+    while (1) {
+      numero = rand () % (0-99999+1) + 99999;
+      //endOfFile = fscanf(firstFile, "%s", str);
+      if (numeros != Nnumeros) {
+        sem_wait(&availableSpaceBuffer1);
+        strcpy(buffer1[nextToFillBuffer1], numero);
+        nextToFillBuffer1 = (nextToFillBuffer1 + 1) % tamBuffer1;
+        sem_post(&availableDataBuffer1);
+        numeros++;
+      } else {
+        break;
+      }
+      numeros = numeros + 1;
+    }
+    fflush(stdout);
+    pthread_exit(NULL);
+  }
+}
+//------------------------------------------------------------------------------
+
+void *consumer(void *arg) {
+  // Declare variable nextToEmptyBuffer1
+  int nextToEmptyBuffer1 = 0;
+  int consumerCounter = 0;
+  // Declare variable nextToFillBuffer2
+  int nextToFillBuffer2 = 0;
+  char result[15];
+  char word[11];
+  // Loop
+  while (1) {
+    // Wait: available data in buffer1
+    sem_wait(&availableDataBuffer1);
+    // Save read data
+    strcpy(word, buffer1[nextToEmptyBuffer1]);
+    // Signal there is available space to write in buffer1
+    // Palindromes function. Save result in global var
+    if (palindromo(word)){
+      sprintf(result, "%s es palindromo", word);
+    } else {
+      sprintf(result, "%s no es palindromo", word);
+    }
+    nextToEmptyBuffer1 = (nextToEmptyBuffer1 + 1) % tamBuffer1;
+    sem_post(&availableSpaceBuffer1);
+    // Wait for available space in buffer2
+    sem_wait(&availableSpaceBuffer2);
+    // Save result in Buffer2
+    strcpy(buffer2[nextToFillBuffer2], result);
+    // Update nextToFillBuffer2 var
+    nextToFillBuffer2 = (nextToFillBuffer2 + 1) % 5;
+    sem_post(&availableDataBuffer2);
+    consumerCounter = consumerCounter + 1;
+    if (consumerCounter == numberOfWords) {
+      break;
+    }
+  }
+  fflush(stdout);
+  pthread_exit(NULL);
+}
+
+
 
 int main(){
    printf("hola");
