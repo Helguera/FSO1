e2715c25f80ddefcb1e2ca20df57574dccbbe83f
Javier Helguera <javier.helguera@alumnos.uva.es>
2016-12-09 19:06:01 +0100

    Problema con la memoria

--- Practica_2016.c
+++ Practica_2016.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 int **buffer1;
 int **buffer2;
-int tamBuffer1;
+
 
 sem_t availableSpaceBuffer1;
 sem_t availableDataBuffer1;
@@ -14,11 +16,26 @@ sem_t availableSpaceBuffer2;
 sem_t availableDataBuffer2;
 
 // Words in file counter
-int wordsCounter = 0;
+int numbersCounter = 0;
 // Final number of file words
-int numberOfWords;
+int numberOfnumbers;
 
 
+
+
+//------------------------------------------------------------------------------
+int esPrimo(int a){
+   int b=0;
+   int res=1;//Es primo
+   int i=2;
+   while(i<a && b==0){
+         if(a%i==0){
+            b=1;
+            res=0;//No es primo
+         }else i++;
+   }
+   return res;
+}
 //------------------------------------------------------------------------------
 
 void *productor(void *arg) {
@@ -27,22 +44,17 @@ void *productor(void *arg) {
   int nextToFillBuffer1 = 0;
   // Productor loop
   while (1) {
-
-    int numeros=0;
-
     while (1) {
-      numero = rand () % (0-99999+1) + 99999;
-      //endOfFile = fscanf(firstFile, "%s", str);
-      if (numeros != Nnumeros) {
+      int numero = rand () % (0-99999+1) + 99999;
+      if (numbersCounter != Nnumeros) {
         sem_wait(&availableSpaceBuffer1);
-        strcpy(buffer1[nextToFillBuffer1], numero);
-        nextToFillBuffer1 = (nextToFillBuffer1 + 1) % tamBuffer1;
+        buffer1[nextToFillBuffer1]=numero;
+        nextToFillBuffer1 = (nextToFillBuffer1 + 1) % tam_buffer;
         sem_post(&availableDataBuffer1);
-        numeros++;
       } else {
         break;
       }
-      numeros = numeros + 1;
+      numbersCounter = numbersCounter + 1;
     }
     fflush(stdout);
     pthread_exit(NULL);
@@ -56,42 +68,44 @@ void *consumer(void *arg) {
   int consumerCounter = 0;
   // Declare variable nextToFillBuffer2
   int nextToFillBuffer2 = 0;
-  char result[15];
-  char word[11];
+  int resultado;
+  int numero;
   // Loop
   while (1) {
     // Wait: available data in buffer1
     sem_wait(&availableDataBuffer1);
     // Save read data
-    strcpy(word, buffer1[nextToEmptyBuffer1]);
+    numero=buffer1[nextToEmptyBuffer1];
     // Signal there is available space to write in buffer1
     // Palindromes function. Save result in global var
-    if (palindromo(word)){
-      sprintf(result, "%s es palindromo", word);
+    if (esPrimo(numero)==1){
+      sprintf(resultado, "%d es primo", numero);
     } else {
-      sprintf(result, "%s no es palindromo", word);
+      sprintf(resultado, "%d no es primo", numero);
     }
-    nextToEmptyBuffer1 = (nextToEmptyBuffer1 + 1) % tamBuffer1;
+    nextToEmptyBuffer1 = (nextToEmptyBuffer1 + 1) % tam_buffer;
     sem_post(&availableSpaceBuffer1);
     // Wait for available space in buffer2
     sem_wait(&availableSpaceBuffer2);
     // Save result in Buffer2
-    strcpy(buffer2[nextToFillBuffer2], result);
+    buffer2[nextToFillBuffer2]=resultado;
     // Update nextToFillBuffer2 var
     nextToFillBuffer2 = (nextToFillBuffer2 + 1) % 5;
     sem_post(&availableDataBuffer2);
     consumerCounter = consumerCounter + 1;
-    if (consumerCounter == numberOfWords) {
+    if (consumerCounter == Nnumeros) {
       break;
     }
   }
   fflush(stdout);
   pthread_exit(NULL);
 }
-
-
+//------------------------------------------------------------------------------
 
 int main(){
+
+
+
    printf("hola");
    return(0);
 }
